852538a665ed2b5da8a50eb94440682dc906f25f
Hamed Gorjiara <hgorjiar@uci.edu>
2022-03-03 02:13:43 -0800

    feat(general): log set up time

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,5 @@
 apt-get update
 apt-get -y install cmake g++ clang pkg-config autoconf pandoc libevent-dev libseccomp-dev xsltproc
 
-su -c /vagrant/data/setup.sh vagrant
+time su -c /vagrant/data/setup.sh vagrant
 
